e245ffe2c030ac9cbc80c5d8f606245f2689507d
Nigel Holder <nholder88@gmail.com>
2019-03-11 00:13:31 -0400

    Added bootstrap
    Need Header,
    No Migraitions done

---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+# Create your views here.
+
+posts = [
+    {
+        'author': 'nigel',
+        'title' : 'Trying out',
+        'content' : 'trfsfdfsgdgsdgsgfdgf',
+        'date' : 'December 25, 2015'
+    },
+    {
+        'author': 'nigel',
+        'title' : 'Trying out 2',
+        'content' : 'trfsfddgf',
+        'date': 'December 26, 2015'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request,'blog/about.html', {'title': 'Passed in'})
+
+
