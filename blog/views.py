7bcf022ebe2107d4d23126647d835a7fce2ba145
Nigel Holder <nholder88@gmail.com>
2019-03-11 00:25:34 -0400

    Formatting Changes
    Signed-off-by: Nigel Holder <nholder88@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render
+
 # Create your views here.
 
 posts = [
     {
         'author': 'nigel',
-        'title' : 'Trying out',
-        'content' : 'trfsfdfsgdgsdgsgfdgf',
-        'date' : 'December 25, 2015'
+        'title': 'Trying out',
+        'content': 'trfsfdfsgdgsdgsgfdgf',
+        'date': 'December 25, 2015'
     },
     {
         'author': 'nigel',
-        'title' : 'Trying out 2',
-        'content' : 'trfsfddgf',
+        'title': 'Trying out 2',
+        'content': 'trfsfddgf',
         'date': 'December 26, 2015'
     }
 ]
+
 
 def home(request):
     context = {
@@ -24,6 +26,4 @@ def home(request):
 
 
 def about(request):
-    return render(request,'blog/about.html', {'title': 'Passed in'})
-
-
+    return render(request, 'blog/about.html', {'title': 'Passed in'})
