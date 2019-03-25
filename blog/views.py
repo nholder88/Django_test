2f7ee7fd390840a1c63e470b170662ca5bbcb15b
Nigel Holder <nholder88@gmail.com>
2019-03-24 21:40:34 -0400

    Updated Blog files

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-posts = [
-    {
-        'author': 'nigel',
-        'title': 'Trying out',
-        'content': 'trfsfdfsgdgsdgsgfdgf',
-        'date': 'December 25, 2015'
-    },
-    {
-        'author': 'nigel',
-        'title': 'Trying out 2',
-        'content': 'trfsfddgf',
-        'date': 'December 26, 2015'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
